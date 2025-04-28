2d08f8a26a5d056e2c1ca147fb1f7816334e8260
mhosseinkaka <mhosseinkaka@gmail.com>
2025-04-28 23:52:16 +0330

    add new permission

--- kelasorBackend/user/permissions.py
+++ kelasorBackend/user/permissions.py
@@ -1,4 +1,6 @@
 from rest_framework.permissions import BasePermission
+from functools import wraps
+from rest_framework.exceptions import PermissionDenied
 
 # class IsSupportUser(BasePermission):
 #     def has_permission(self, request, view):
@@ -32,3 +34,32 @@ class IsInStudentGroup(BasePermission):
             return False
 
         return request.user.groups.filter(name=self.group_name).exists()
+    
+
+
+# def group_required(group_name):
+#     """
+#     دکوریتور برای چک کردن اینکه آیا کاربر عضو گروه خاصی هست یا نه.
+#     """
+#     def decorator(view_class):
+#         class GroupPermission(view_class):
+#             def get_permissions(self):
+#                 permissions = super().get_permissions()
+#                 permissions.append(GroupMembershipPermission(group_name))
+#                 return permissions
+#         return GroupPermission
+#     return decorator
+
+# class GroupMembershipPermission(BasePermission):
+#     """
+#     پرمیشن برای چک عضویت در گروه
+#     """
+#     def __init__(self, group_name):
+#         self.group_name = group_name
+
+#     def has_permission(self, request, view):
+#         if not request.user.is_authenticated:
+#             return False
+#         if not request.user.groups.filter(name=self.group_name).exists():
+#             raise PermissionDenied(f"شما دسترسی به این بخش ({self.group_name}) را ندارید.")
+#         return True
