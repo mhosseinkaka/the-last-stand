0ccba59c7b8c7757966c46a3cbd66885df3d779b
mr.kaka <est4.mh.kaka@gmail.com>
2025-04-28 17:28:25 +0330

    finish urls

--- kelasorBackend/bootcamp/urls.py
+++ kelasorBackend/bootcamp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from bootcamp.views import BootcampListCreateView, BootcampRetrieveUpdateDestroyView
+from bootcamp.views import BootcampCreateView, BootcampRetrieveUpdateDestroyView, BootcampAdminListView, BootcampListView
 
 urlpatterns = [
-    path('create-bootcamp/', BootcampListCreateView.as_view(), name='bootcamp-list-create'),
+    path('create-bootcamp/', BootcampCreateView.as_view(), name='bootcamp-list-create'),
     path('bootcamp/<int:pk>/', BootcampRetrieveUpdateDestroyView.as_view(), name='bootcamp-detail'),
+    path('bootcamp-admin-list/', BootcampAdminListView.as_view(), name='bootcamp-admin-list'),
+    path('bootcamp-list/', BootcampListView.as_view(), name='bootcamp-list'),
 ]
