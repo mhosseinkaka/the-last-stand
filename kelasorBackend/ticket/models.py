9380d00a0d2f69136a219bc9b43e0ec332acc64e
mhosseinkaka <mhosseinkaka@gmail.com>
2025-05-06 21:29:18 +0330

    add image field & file field

--- kelasorBackend/ticket/models.py
+++ kelasorBackend/ticket/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from user.models import User
+from bootcamp.models import Bootcamp
 
 # Create your models here.
 STATUS_CHOICES = (
@@ -13,9 +14,12 @@ STATUS_CHOICES = (
 class Ticket(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='tickets')
     subject = models.CharField(max_length=255)
+    bootcamp = models.ForeignKey(to=Bootcamp, on_delete=models.PROTECT, null=True, blank=True)
     message = models.TextField()
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
     created_at = models.DateTimeField(auto_now_add=True)
+    ticket_image = models.ImageField(upload_to='ticket_images', null=True, blank=True)
+    ticket_file = models.FileField(upload_to='ticket_files', null=True, blank=True)
 
     def __str__(self):
         return f"{self.subject} - {self.user.phone}"
