4fb164d476308b167e9305296a44aaab32fda293
mhosseinkaka <mhosseinkaka@gmail.com>
2025-05-22 12:08:15 +0330

    register model

--- kelasorBackend/blog/admin.py
+++ kelasorBackend/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from blog.models import BlogPost, BlogImage, Tag
 # Register your models here.
+
+admin.site.register(BlogPost)
+admin.site.register(BlogImage)
+admin.site.register(Tag)
